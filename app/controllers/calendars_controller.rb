afc4578cb90f17c7e07ae12620360448d812c161
wiz-program <ywizmida@outlook.jp>
2023-08-10 19:57:10 +0900

    曜日の修正

--- app/controllers/calendars_controller.rb
+++ app/controllers/calendars_controller.rb
@@ -34,7 +34,7 @@ class CalendarsController < ApplicationController
       plans.each do |plan|
         today_plans.push(plan.plan) if plan.date == @todays_date + x
       end
-      days = { month: (@todays_date + x).month, date: (@todays_date+x).day, plans: today_plans, day: @wdays[x] }
+      days = { month: (@todays_date + x).month, date: (@todays_date+x).day, plans: today_plans, day: @wdays[@todays_date.wday] }
 
       @week_days.push(days)
     end
